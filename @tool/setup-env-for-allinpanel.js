b6acf403ee7658f8f2a09e5a665be06c6e7786f6
gxs <lyricsong47@gmail.com>
2025-06-15 19:09:48 +0800

    init

--- @tool/setup-env-for-allinpanel.js
+++ @tool/setup-env-for-allinpanel.js
@@ -2,12 +2,12 @@ const { join, resolve } = require('path');
 
 const srcPath = global.adminPath;
 if (!srcPath) {
-    throw new Error('"globals.adminPath" is not defined. A file path to a Allinpanel 6 administration is required');
+    throw new Error('"globals.adminPath" is not defined. A file path to a AllinPanel 6 administration is required');
 }
 
 global.window._features_ = {};
 
-const Allinpanel = require(resolve(join(srcPath, `src/core/allinpanel.ts`))).AllinpanelInstance;
+const AllinPanel = require(resolve(join(srcPath, `src/core/allinpanel.ts`))).AllinPanelInstance;
 
 const envBefore = process.env.NODE_ENV;
 
@@ -17,13 +17,13 @@ process.env.NODE_ENV = 'production';
 // Enable Pinia Support
 const { createApp } = require(resolve(join(srcPath, 'node_modules/vue/dist/vue.cjs.js')));
 const app = createApp();
-app.use(Allinpanel.Store._rootState)
+app.use(AllinPanel.Store._rootState)
 
 // Reset NODE_ENV to previous value
 process.env.NODE_ENV = envBefore;
 
 module.exports = (() => {
-    global.Allinpanel = Allinpanel;
+    global.AllinPanel = AllinPanel;
     require(resolve(join(srcPath, 'src/app/mixin/index'))).default(); // eslint-disable-line
     require(resolve(join(srcPath, 'src/app/directive/index'))).default(); // eslint-disable-line
     require(resolve(join(srcPath, 'src/app/filter/index'))).default(); // eslint-disable-line
